2e476793a70cac7f9c78cf82061eeab0db88726f
me-lw-in <melwinmanish30@gmail.com>
2025-07-03 19:32:13 +0530

    Initial commit: set up Spring Boot project with dependencies and migration file

---
+++ src/test/java/com/melwin/qrmenu/QrMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melwin.qrmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
